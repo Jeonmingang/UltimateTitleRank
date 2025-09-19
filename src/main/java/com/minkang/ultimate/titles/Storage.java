9bee29016be6da7f28a9f58906024cf03b75c98e
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-20 06:41:37 +0900

    Delete src/main directory

--- src/main/java/com/minkang/ultimate/titles/Storage.java
+++
@@ -1,9 +0,0 @@
-package com.minkang.ultimate.titles;
-
-import java.util.UUID;
-
-public interface Storage {
-    PlayerData get(UUID uuid);
-    void save(UUID uuid, PlayerData data);
-    int purgeSlotAllPlayers(int slot);
-}
